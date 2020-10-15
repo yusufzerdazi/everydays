59f6a77bd37e5b3380236ed625e026f88369f243
Yusuf Zerdazi <yusuf@zerdazi.com>
2020-10-15 19:21:49 +0100

    Fixing the consolidator

--- EverydayConsolidator.cs
+++ EverydayConsolidator.cs
@@ -18,14 +18,16 @@ namespace Everydays
         private static BlobContainerClient _containerClient;
 
         [FunctionName("EverydayConsolidator")]
-        public static async Task Run([TimerTrigger("0 0 1 * * *")] TimerInfo myTimer, string name, ILogger log)
+        public static async Task Run([TimerTrigger("0 0 1 * * *")] TimerInfo myTimer, ILogger log)
         {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             // Create a BlobServiceClient object which will be used to create a container client
             BlobServiceClient blobServiceClient = new BlobServiceClient(Environment.GetEnvironmentVariable("EverydaysStorageConnectionString"));
             _containerClient = blobServiceClient.GetBlobContainerClient("everydays");
 
             var everydays = new List<InstagramPost>();
             await foreach(var blob in _containerClient.GetBlobsAsync(prefix: "data")){
+                log.LogInformation($"Downloading: {blob.Name}");
                 var everydayBlobClient = _containerClient.GetBlobClient(blob.Name);
                 var blobContent = await everydayBlobClient.DownloadAsync();
                 var everydayReader = new StreamReader(blobContent.Value.Content);
@@ -37,6 +39,7 @@ namespace Everydays
             byte[] byteArray = Encoding.UTF8.GetBytes(everydaysString);
             MemoryStream stream = new MemoryStream(byteArray);
 
+            log.LogInformation($"Updating consolidated blob.");
             var blobClient = _containerClient.GetBlobClient($"everydays.json");
             await blobClient.UploadAsync(stream, overwrite: true);
         }
